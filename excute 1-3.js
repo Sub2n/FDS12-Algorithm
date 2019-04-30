34eb2395e6dfe7f299129288ea1ec3e9264186b0
Sub2n <95su1208@gmail.com>
2019-04-30 15:57:27 +0900

    feat: nn

--- excute 1-3.js
+++ excute 1-3.js
@@ -10,7 +10,7 @@
 function insensitiveEqual(string1, string2){
     if(string1.localeCompare(string2, undefined, {sensitivity: 'accent'}) === 0)
         return true;
-    else return false;
+    return false;
 }
 
 
@@ -22,28 +22,26 @@ function insensitiveEqual(string1, string2){
 // leftPad('hello', 8); -> '   hello'
 // leftPad('hello', 3); -> 'hello'
 
-function leftPad(string, number){
-    if(string.length >= number) return string;
-    else{
+function leftPad (string, number) {
+    if (string.length >= number) return string;
+    
         while(string.length < number){
             string = ' ' + string;
         }
         return string;
-    }
+    
 }
 
 // 문제 3
 // 문자열을 입력받아, 문자열 안에 들어있는 모든 모음(a, e, i, o, u)의 갯수를 반환하는 함수를 작성하세요.
 
-function vowelNum(string){
-    var vowels = new Set(['a', 'e', 'i', 'o', 'u']);
-    var num = 0;
-    for(i in string){
-      if(vowels.has(string[i])){
-        num ++;
-      }
+function vowelNum(string) {
+  var vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+  var num = 0;
+  for (i in string) {
+    if (vowels.has(string[i])) {
+      num++;
     }
-    return num;
+  }
+  return num;
 }
-
-
